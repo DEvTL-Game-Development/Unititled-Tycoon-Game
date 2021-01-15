0d3d392fd0725533d3c8b0490e498ce7baedd8e6
john646 <77420223+john646@users.noreply.github.com>
2021-01-15 12:27:29 -0500

    Update cammove.cs
    Updates the names

--- cammove.cs
+++ cammove.cs
@@ -4,9 +4,9 @@
 
 public class cammove : MonoBehaviour
 {
-    public float panspeed = 20;
+    public float panSpeed = 20;
     public float panBorderThickness = 10f;
-    public Vector2 panlimit;
+    public Vector2 panLimit;
 
     public float scrollSpeed = 20f;
     public float minY = 10f;
@@ -19,29 +19,29 @@ public class cammove : MonoBehaviour
 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
-            pos.z += panspeed * Time.deltaTime; 
+            pos.z += panSpeed * Time.deltaTime; 
         }
 
         if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
         {
-            pos.z -= panspeed * Time.deltaTime; 
+            pos.z -= panSpeed * Time.deltaTime; 
         }
 
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            pos.x += panspeed * Time.deltaTime; 
+            pos.x += panSpeed * Time.deltaTime; 
         }
 
         if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
         {
-            pos.x -= panspeed * Time.deltaTime; 
+            pos.x -= panSpeed * Time.deltaTime; 
         }
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
 
-        pos.x = Mathf.Clamp(pos.x, -panlimit.x, panlimit.x);
-        pos.z = Mathf.Clamp(pos.z, -panlimit.y, panlimit.y);
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
 
         transform.position = pos;
         
