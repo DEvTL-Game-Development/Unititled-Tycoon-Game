0c6e60b836142f8f616c884f9fa75958d2f7cdbd
fastFox2 <72040701+fastFox2@users.noreply.github.com>
2021-01-16 09:12:03 +0100

    Script now follows our conventions.

--- cammove.cs
+++ cammove.cs
@@ -1,16 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-//Attach script to camera and set values
+/// <summary>
+/// This Script handles the camera movement. Attach the script to the camera and set the values
+/// </summary>
 
-public class cammove : MonoBehaviour
+class CameraMovement : MonoBehaviour
 {
-    public float panSpeed = 20;
-    public float panBorderThickness = 10f;
-    public Vector2 panLimit;
+    [SerializeField]
+    private float panSpeed = 20;
+    [SerializeField]
+    private float panBorderThickness = 10f;
 
-    public float scrollSpeed = 20f;
-    public float minY = 10f;
-    public float maxY = 10f;
+    [SerializeField]
+    private float scrollSpeed = 20f;
+    [SerializeField]
+    private float minY = 10f;
+    [SerializeField]
+    private float maxY = 10f;
 
     // Update is called once per frame
     void Update()
@@ -19,31 +25,32 @@ public class cammove : MonoBehaviour
 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
-            pos.z += panSpeed * Time.deltaTime; 
+            pos.z += panSpeed * Time.deltaTime;
+            Debug.Log("Pressed w");
         }
 
         if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
         {
-            pos.z -= panSpeed * Time.deltaTime; 
+            pos.z -= panSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            pos.x += panSpeed * Time.deltaTime; 
+            pos.x += panSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
         {
-            pos.x -= panSpeed * Time.deltaTime; 
+            pos.x -= panSpeed * Time.deltaTime;
         }
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
 
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+        //pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        //pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
 
         transform.position = pos;
-        
+
     }
 }
